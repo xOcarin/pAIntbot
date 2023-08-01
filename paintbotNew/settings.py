307843f5d6d99defa0eddc77fc5aebef324b79d9
xOcarin <119756949+xOcarin@users.noreply.github.com>
2023-08-01 01:23:24 -0400

    new

--- paintbotNew/settings.py
+++ paintbotNew/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2sr#3%b%@z(=g8y-zu(sm%kxkk1v1daok^nhv!^=aaw20qco1n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app']
 
 
 # Application definition
